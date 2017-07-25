8d28958a70463b245a23ecfaa1786c6adc03c9ad
Morgan Rhodes <morgan@puppet.com>
2017-07-25 00:09:23 +0000

    temp debby things

--- controller.sh
+++ controller.sh
@@ -19,3 +19,6 @@ time ruby ~/fpm.rb $extras --debug --operating-system suse --os-version 1315 --d
 time ruby ~/fpm.rb $extras --debug --operating-system el --os-version 6 --name puppetserver --package-version 5.0.0 --release 4 --additional-dependency "puppet-agent >= 4.99.0" --user puppet --group puppet --chdir old_el --source etc,opt,var
 
 cd -
+
+# temporary, deb working!
+fpm --output-type deb --input-type dir --name puppetserver --deb-user root --deb-group root --template-scripts --template-value 'user=puppet' --template-value 'group=puppet' --template-value 'project=puppetserver' --template-value 'real_name=puppetserver' --before-install ~/ezbake/deb-pre.sh.erb --after-install ~/ezbake/deb-post.sh.erb --force --depends openjdk-8-jre-headless --depends net-tools --depends adduser --depends procps --depends 'puppet-agent >= 4.99.0' etc lib opt usr var
